133119a8c5616b0e1e771242857a75513c1f914e
Антон Голенцов <angolenc@yandex.ru>
2022-06-29 19:59:27 +0300

    change permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,15 +2,17 @@ from rest_framework import permissions
 
 
 def check_post(request):
-    roles = ['user', 'moderator', 'admin']
-    return (request.method == 'POST' and request.user.role in roles)
+    return (request.method == 'POST'
+            and (request.user.is_user
+                 or request.user.is_moderator
+                 or request.user.is_admin))
 
 
 def check_patch_delete(request, obj):
-    roles = ['moderator', 'admin']
     methods = ['PATCH', 'DELETE']
     return (request.method in methods
-            and (request.user.role in roles
+            and (request.user.is_admin
+                 or request.user.is_moderator
                  or request.user == obj.author))
 
 
@@ -41,4 +43,4 @@ class IsAdminOrReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_superuser or request.user.is_authenticated
-                and request.user.role == 'admin')
+                and request.user.is_admin)
